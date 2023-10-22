33e67c6babcb6c0cf7a8a828d780e91f8477b2c4
imakepizza <anpash1317@gmail.com>
2023-10-22 14:15:39 +0900

    fix lint

--- src/init.js
+++ src/init.js
@@ -113,7 +113,7 @@ const init = () => {
           .then((link) => {
             watchedState.form.process = 'sending';
             const allOriginsURL = getAllOriginsURL(link);
-            return axios.get(allOriginsURL, { timeout: 5000 });
+            return axios.get(allOriginsURL);
           }).then((response) => {
             const responseContent = response.data.contents;
             const { feed, posts } = parse(responseContent);
